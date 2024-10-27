fd51088a266072270d2aaeebc8e29d9a3329d943
RaminEmrahli <raminemrahli53@gmail.com>
2024-10-27 17:05:41 +0400

    Entities Created

---
+++ Core/Ecommerce.Domain/Entities/Category.cs
@@ -0,0 +1,31 @@
+﻿using Ecommerce.Domain.Common;
+using Ecommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Domain.Entities
+{
+    public class Category : EntityBase
+    {
+        public Category()
+        {
+
+        }
+        public Category(int parentId, string name, int priority)
+        {
+            ParentId = parentId;
+            Name = name;
+            Priority = priority;
+        }
+
+        public required int ParentId { get; set; }
+        public required string Name { get; set; }
+        public required int Priority { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<ProductCategory> ProductCategories { get; set; }
+    }
+    
+}
