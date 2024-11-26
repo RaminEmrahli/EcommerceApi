977e44905db7914c36194e25b3902760eba67bf4
RaminEmrahli <raminemrahli53@gmail.com>
2024-11-26 18:42:47 +0400

    Redis Services Created

---
+++ Core/Ecommerce.Application/Behaviours/RedisCacheBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using Ecommerce.Application.Interfaces.RedisCache;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Application.Behaviours
+{
+    public class RedisCacheBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IRedisCacheService redisCacheService;
+
+        public RedisCacheBehaviour(IRedisCacheService redisCacheService)
+        {
+            this.redisCacheService = redisCacheService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (request is ICacheableQuery query)
+            {
+                var cacheKey = query.CacheKey;
+                var cacheTime = query.CacheTime;
+
+                var cachedData = await redisCacheService.GetAsync<TResponse>(cacheKey);
+                if(cachedData is not null)
+                {
+                    return cachedData;
+                }
+                var response = await next();
+                if(response is null)
+                {
+                    await redisCacheService.SetAsync<TResponse>(cacheKey,response,DateTime.Now.AddMinutes(cacheTime));
+                    return response;
+                }
+            }
+            return await next();
+        }
+    }
+}
---
+++ Infrastructure/Ecommerce.Infrastructure/RedisCache/RedisCacheService.cs
@@ -0,0 +1,42 @@
+﻿using Ecommerce.Application.Interfaces.RedisCache;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.RedisCache
+{
+    public class RedisCacheService : IRedisCacheService
+    {
+        private readonly IDatabase database;
+        private readonly ConnectionMultiplexer redisConnection;
+        private readonly RedisCachesSettings settings;
+        public RedisCacheService(IOptions<RedisCachesSettings> options)
+        {
+            settings = options.Value;
+            ConfigurationOptions opt = ConfigurationOptions.Parse(settings.ConnectionString);
+            redisConnection = ConnectionMultiplexer.Connect(opt);
+            database = redisConnection.GetDatabase();
+        }
+        public async Task<T> GetAsync<T>(string key)
+        {
+            var value= await database.StringGetAsync(key);
+            if (value.HasValue)
+            {
+                return JsonConvert.DeserializeObject<T>(value);
+            }
+            return default;
+        }
+
+        public async Task SetAsync<T>(string key, T value, DateTime? expirationTime = null)
+        {
+            TimeSpan timeUntilExpiration = expirationTime.Value - DateTime.Now;
+            await database.StringSetAsync(key, JsonConvert.SerializeObject(value), timeUntilExpiration);
+
+        }
+    }
+}
